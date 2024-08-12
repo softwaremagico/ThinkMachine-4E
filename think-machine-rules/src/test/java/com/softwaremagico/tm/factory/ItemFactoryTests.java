46c028332bffbe55a07a71aa9509da823a288132
softwaremagico <softwaremagico@gmail.com>
2024-08-12 08:30:23 +0200

    Added the dispossessed

--- think-machine-rules/src/test/java/com/softwaremagico/tm/factory/ItemFactoryTests.java
+++ think-machine-rules/src/test/java/com/softwaremagico/tm/factory/ItemFactoryTests.java
@@ -31,7 +31,7 @@ import org.testng.annotations.Test;
 
 @Test(groups = {"itemFactory"})
 public class ItemFactoryTests {
-    private static final int DEFINED_ITEMS = 12;
+    private static final int DEFINED_ITEMS = 13;
 
     @Test
     public void readItems() throws InvalidXmlElementException {
